a66cd8f7f9ece62726924877ab42c43c8cfed34a
SWLivingstone <scottwlivingstone@gmail.com>
2017-08-01 20:57:31 -0700

    Checkpoint 1 Intro to data structures.

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,47 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @screen = create_grid(width, height)
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    if inbounds(x, y)
+      @screen[y][x] = pixel
+    else
+      puts "Those pixels are not valid"
+    end
   end
 
   def at(x, y)
+    if inbounds(x, y)
+      @screen[y][x]
+    else
+      nil
+    end
   end
 
   private
 
+  def create_grid(width, height)
+    grid = []
+    height.times do |i|
+      grid[i] = []
+      width.times do
+        grid[i].push([])
+      end
+    end
+    return grid
+  end
+
   def inbounds(x, y)
+    if x < 0 or y < 0
+      false
+    elsif y > @screen.length or x > @screen[0].length
+      false
+    else
+      true
+    end
   end
 
 end
